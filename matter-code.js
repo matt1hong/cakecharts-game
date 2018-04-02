e32d41cc852c5d8b2c9c55de52a21004126d40b9
Matt Hong <mhong19414@gmail.com>
2018-04-02 13:35:14 -0400

    stick worm to ground properly

--- matter-code.js
+++ matter-code.js
@@ -4,26 +4,37 @@ var ball;
 var block;
 var floor;
 
-function checkBoundaries() {
-  var pos = block.getPositionX();
-  if ((pos < margin && spriteDir === -1 && !block.isFrozen())
-    || (pos > width - margin && spriteDir === 1 && !block.isFrozen())) {
-    spriteDir = 0;
-    block.freeze();
-  } else if (spriteDir === 1 || spriteDir === -1) {
-    block.unfreeze()
-    block.setVelocityX(spriteDir*0.7) 
-  }
-}
-
 var prices = [2.99, 4.00, 1.00, 3.5];
-var interval = 100;
+var interval = 1;
 //  Lines are divided into this many total parts, so that each can be partially destroyed
-var totalSegments = 1;
+var totalSegments = 20;
 var segmentLineBy = Math.max(1,totalSegments/prices.length);
+var morePrices = interpolateBetween(prices,100)
 // var numLineParts = totalSegments/
 var lines = [];
 var clicked = [];
+
+
+function findCurrentLine(ball) { 
+	var posX = ball.body.position.x
+	return lines.filter(function(x){
+		return x.body.bounds.min.x<posX && x.body.bounds.max.x>posX
+	})[0]
+}
+
+function interpolateBetween(arr, howMany) {
+	var newArr = []
+	for (var i = 0; i < arr.length-1; i++) {
+		var tempArr = []
+		var interval = (arr[i+1] - arr[i])/howMany
+		for (var j = 0; j < howMany; j++) {
+			tempArr.push(arr[i] + interval * j)
+		}
+		newArr = newArr.concat(tempArr)
+	}
+	newArr.push(arr[arr.length-1])
+	return newArr
+}
 
 function centerLine(x1,y1,x2,y2) {
   var centerX = (x1+x2)/2, centerY = (y1+y2)/2
@@ -33,17 +44,17 @@ function centerLine(x1,y1,x2,y2) {
 }
 
 function lineChart() {
-  var lastPrice = height - map(prices[0], 0, 5, 0, 100);
+  var lastPrice = height - map(morePrices[0], 0, 5, 0, 100);
   var x1 = 0;
-  for (var i=1; i<prices.length; i++) {
+  for (var i=1; i<morePrices.length; i++) {
     x1 = (i-1)*interval;
-    adjustedPrice = height - map(prices[i], 0, 5, 0, 100);
+    adjustedPrice = height - map(morePrices[i], 0, 5, 0, 100);
     if (clicked.indexOf(x1) < 0) {
       centered = centerLine(
         x1, 
         lastPrice, 
-        x1+(interval/segmentLineBy), 
-        lastPrice+(adjustedPrice-lastPrice)/segmentLineBy
+        i*interval, 
+        adjustedPrice
       )
       lines.push(matter.makeBarrier(
         centered[0], centered[1], centered[2], 1, {
--- sketch.js
+++ sketch.js
@@ -1,29 +1,12 @@
-
-
 //p5
-
-  var mx = 100;
-  var val = {
-    v: 100,
-    get val() {
-      return Math.min(Math.max(this.v, 0), mx)
-    },
-    set val(v) {
-      if (v) this.v = v
-    }
-  }
-  var angle = 0;
-  var ww = 50;
-  var hh = 500;
-  var balls = [];
 
 class Shooter {
   constructor(ball) {
     this.ball = ball
     this.angle = 0
     this.r = 30
-    this.bulletRadius = 40
-    this.direction = 1
+    this.bulletRadius = 20
+    this.direction = 0
     this.powerBar = new PowerBar()
     this.bullets = []
   }
@@ -37,11 +20,11 @@ class Shooter {
   }
 
   get muzzleX() {
-    return this.ball.getPositionX() + r * Math.cos(radians(this.angle)) + bulletRadius * Math.cos(radians(angle))
+    return this.ball.getPositionX() + (this.r + 10) * Math.cos(radians(this.angle)) + this.bulletRadius * Math.cos(radians(this.angle))
   }
 
   get muzzleY() {
-    return this.ball.getPositionY() + r * Math.sin(radians(this.angle)) + bulletRadius * Math.sin(radians(angle))
+    return this.ball.getPositionY() + (this.r + 10) * Math.sin(radians(this.angle)) + this.bulletRadius * Math.sin(radians(this.angle))
   }
 
   aim() {
@@ -60,9 +43,13 @@ class Shooter {
       x: this.muzzleX,
       y: this.muzzleY
     }, {
-      x: Math.cos(bullet.body.angle)*(100-this.powerBar.level)/100,
-      y: Math.sin(bullet.body.angle)*(100-this.powerBar.level)/100
+      x: Math.cos(bullet.body.angle)*(100-this.powerBar.level)/2000,
+      y: Math.sin(bullet.body.angle)*(100-this.powerBar.level)/2000
     })
+    setTimeout(function(){
+      s.restart({x:bullet.getPositionX(), y:bullet.getPositionY()})
+      matter.forget(bullet)
+    }, 5000)
   }
 
   checkForWall() {
@@ -74,13 +61,20 @@ class Shooter {
   }
 
   move() {
-    this.checkForWall()
-    if (this.direction === 0 && !this.ball.isFrozen()) {
-      this.ball.freeze()
-    } else if (this.direction === 1 || this.direction === -1) {
-      this.ball.unfreeze()
+    if (!this.ball.isFrozen()) {
+      this.checkForWall()
+    }
+    if (this.direction === 1 || this.direction === -1) {
       this.ball.setVelocityX(this.direction*0.7) 
     }
+  }
+
+  show() {
+    push();
+    translate(this.x, this.y)
+    rotate(radians(this.angle))
+    image(mcSpicy, 0, 0, 75, 75)
+    pop();
   }
 }
 
@@ -129,20 +123,48 @@ class PowerBar {
     if (keyIsDown(32)) {
       if (this.level === 100) {
         this.direction = -1 
-      } else if (n === 1) {
+      } else if (this.level === 1) {
         this.direction = 1
       }
       this.level = this.level + this.direction
+    } else {
+      this.level = 100
     }
   }
+
+  show() {
+    push();
+    fill(222);
+    rect(20, 0, this.width, this.height);
+    // Draw the fill
+    fill(111);
+    var fillVal = Math.min(Math.max(this.level/this.max, 0), 1);
+
+    rect(20, 0, this.width, fillVal * this.height);
+    pop()
+  }
 }
+
+
+
+
 
 function preload() {
   mcSpicy = loadImage("mcspicy.png");
 }
 
+let player1;
+let conn;
+var s;
+
 function setup() {
+  var a = ['#666666']
+  for (var i = 3; i >= 0; i--) {
+    a = a.concat(a)
+  }
   // put setup code here.
+  s = new Splasher({x:200,y:200},a)
+
   createCanvas(600, 600);
 
   matter.init();
@@ -150,110 +172,180 @@ function setup() {
   line(20,20,200,200);
   imageMode(CENTER);
 
-  block = matter.makeBall(200, 300, 30);
-  
-  //Add a title
-  fill('#000');
-  noStroke();
-  textSize(15);
-  // floor = matter.makeBarrier(width / 2, height, width, 50);
-}
 
-var progBarDir = 1
-function goUpOrDown (n) {
-  if (n === 100) {
-    progBarDir = -1 
-  } else if (n === 1) {
-    progBarDir = 1
-  }
-  return n + progBarDir
-}
-
-function aimCannon() {
-  if (keyIsDown(UP_ARROW)) { 
-    angle = angle - 1
-  } else if (keyIsDown(DOWN_ARROW)) { 
-    angle = angle + 1
-  }
+  player1 = new Shooter(matter.makeBall(200, 400, 40));
 }
 
 function keyPressed() {
-  var arrowMap = {};
-
-  if (keyCode === LEFT_ARROW || keyCode === RIGHT_ARROW) {
-    arrowMap[LEFT_ARROW] = -1;
-    arrowMap[RIGHT_ARROW] = 1;
-    spriteDir = arrowMap[keyCode];
+  if (keyCode === LEFT_ARROW) {
+    if (conn) matter.forget(conn)
+    if (player1.ball.isFrozen()) {
+      player1.ball.unfreeze() 
+    }
+    player1.direction = -1
+  } else if (keyCode === RIGHT_ARROW) {
+    if (conn) matter.forget(conn)
+    if (player1.ball.isFrozen()) {
+      player1.ball.unfreeze() 
+    }
+    player1.direction = 1
   }
-
 }
 
 function keyReleased() {
   if (keyCode === LEFT_ARROW || keyCode === RIGHT_ARROW) {
-    spriteDir = 0
-    if (!block.isFrozen()) {
-      block.freeze();
+    player1.direction = 0
+    var currentLine = findCurrentLine(player1.ball)
+    conn = matter.connect(player1.ball, currentLine)
+    if (!player1.ball.isFrozen()) {
+      player1.ball.freeze() 
     }
   }
-  var xx = block.getPositionX() + 30 * Math.cos(radians(angle));
-  var yy = block.getPositionY() + 30 * Math.sin(radians(angle));
   if (keyCode ===32) {
-    var ball = matter.makeBall(xx + 40 * Math.cos(radians(angle)), yy + 40 * Math.sin(radians(angle)), 80,{restitution:0.9})
-    balls.push(ball)
-    Matter.Body.setAngle(ball.body, radians(angle))
-    Matter.Body.applyForce(ball.body, {
-      x: xx,
-      y: yy
-    }, {
-      x: Math.cos(ball.body.angle)*(100-val.v)/100,
-      y: Math.sin(ball.body.angle)*(100-val.v)/100
-    })
+    player1.shoot();
   }
 }
+
+
+var Particle = function(x, y, attributes){
+
+  this.pos = createVector(x, y);
+  this.vel = createVector(0, 0);
+  this.acc = createVector(0, 0);
+  this.attributes = attributes;
+
+  this.update = function(){
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc = createVector(0, 0);
+  }
+
+  this.draw = function(drawer){
+    drawer(this);
+  }
+
+  this.applyForces = function(forces){
+    if(!Array.isArray(forces)){
+      this.acc.add(forces);
+    }else{
+      for(var i = 0; i < forces.length; i++){
+        this.acc.add(forces[i]);
+      }
+    }
+    
+  }
+}
+
+var Splasher = function(pos, colors){
+
+  this.startPos = pos;
+  this.colors = colors;
+  this.particles = [];
+  this.active = true;
+
+  this.init = function(){
+
+
+    var xBaseForce = 5;
+    var yBaseForce = 2;
+
+    var yOff = 0;
+    var xOff = 0;
+
+    for(var i = 0; i < this.colors.length; i++){
+      
+      var particle = new Particle(this.startPos.x, this.startPos.y,
+        {
+          radius : Math.random()*15,
+          color : colors[i].slice()
+        }
+      );
+
+      yOff += 0.5;
+      xOff += 0.7;
+
+      particle.applyForces(createVector(map(noise(xOff), 0, 1, -1, 1) * xBaseForce, -noise(yOff) * yBaseForce));
+      this.particles.push(particle);
+    }
+  }
+
+  this.init();
+
+  this.update = function(){
+    if(this.active){
+      for(var i = 0; i < this.particles.length; i++){
+        this.particles[i].applyForces([
+          createVector(0, .03 * this.particles[i].attributes.radius) //gravity
+        ]); 
+        //this.particles[i].attributes.color[3] -= 2;
+        this.particles[i].update();
+      }
+    }
+  }
+
+  this.draw = function(){
+    if(this.active){
+      for(var i = 0; i < this.particles.length; i++){
+        this.particles[i].draw(drawParticle);
+      }
+    }
+  }
+
+  /*
+    Function passed as a drawer for particle draw method.
+  */
+  var drawParticle = function(particle){
+    push();
+    noStroke();
+    fill(particle.attributes.color);
+    ellipse(particle.pos.x, particle.pos.y, particle.attributes.radius);
+    pop();
+  }
+
+  this.start = function(){
+    this.active = true;
+  }
+
+  this.reload = function(){
+    this.particles = [];
+    this.active = false;
+    this.init();
+  }
+
+  this.restart = function(pos){
+    if (pos) {
+      this.startPos = pos;
+    }
+    this.reload();
+    this.start();
+  }
+}
+
+
 
 function draw() {
   // put the drawing code here
-  background(255);
+  background(255)
+  axes()
+  for (var i = lines.length - 1; i >= 0; i--) {
+    lines[i].show()
+  }
+  s.update();
+  s.draw();
 
-  fill(127);
-  block.show();
-  for (var j = balls.length - 1; j >= 0; j--) {
-    var ball = balls[j];
-    ball.show();
-    if (ball.isOffCanvas()) {
-      matter.forget(ball);
+  player1.show()
+  player1.move()
+  player1.powerBar.show()
+  player1.powerBar.progress()
+  player1.aim()
+  for (var j = player1.bullets.length - 1; j >= 0; j--) {
+    var bullet = player1.bullets[j]
+    if (bullet.active) {
+      bullet.show()
+    }
+    if (bullet.isOffCanvas()) {
+      matter.forget(bullet)
     }
   }
-
-  push();
-  translate(block.getPositionX(), block.getPositionY())
-  rotate(radians(angle))
-  image(mcSpicy, 0, 0, 75, 75)
-  pop();
-  axes();
-  // image(moonImg, moon.getPositionX(), moon.getPositionY());
-    // Draw the background
-  push();
-  fill(222);
-  rect(20, 0, ww, hh);
-  // Draw the fill
-  fill(111);
-  var fillVal = Math.min(Math.max(val.v/ mx, 0), 1);
-
-  rect(20, 0, ww, fillVal * hh);
-  pop()
-  if (keyIsDown(32)) { // Spacebar
-    val.v = goUpOrDown(val.v);
-  }
-  aimCannon()
-
-  for (var i = lines.length - 1; i >= 0; i--) {
-    lines[i].show();
-  }
-  checkBoundaries()
-
-  // ball.show();
 }
-
-
 
